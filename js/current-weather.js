3d5f68165e5dd474519a3487803ea9ba7b0a259a
Esteban Pajares <jehupajares@gmail.com>
2022-05-29 00:06:37 -0500

    Agregando la funcion configCurrentWeather al proyecto

--- js/current-weather.js
+++ js/current-weather.js
@@ -1,6 +1,14 @@
 import weather from '../data/current-weather.js' //importando data del clima
-
+function configCurrentWeather(weather){
+    //loader
+    //dayWeather
+    //city
+    //temp
+    //background
+}
 export default function currentWeather(){
+    //GEO //API - weather //config
+    configCurrentWeather()
     console.log(weather)
 
 }
