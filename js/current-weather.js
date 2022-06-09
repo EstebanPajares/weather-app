d426de0fb794c7fdb57561d295c0e20712fb62f2
Esteban Pajares <jehupajares@gmail.com>
2022-06-09 00:14:45 -0500

    Renderizando la ciudad de origen del proyecto

--- js/current-weather.js
+++ js/current-weather.js
@@ -1,14 +1,27 @@
 import weather from '../data/current-weather.js' //importando data del clima
+
+function setCurrentCity($el, city){ //contenedor y ciudad
+    $el.textContent = city
+}
+
 function configCurrentWeather(weather){
     //loader
     //dayWeather
     //city
+    const $currentWeatherCity = document.querySelector('#current-weather-city')
+    const city = weather.name
+
+    setCurrentCity($currentWeatherCity, city)
+    
+    //const $currentWeatherCity = document.querySelector('#current-weather-city')
+    //$currentWeatherCity.textContent = weather.name
+    debugger
     //temp
     //background
 }
 export default function currentWeather(){
     //GEO //API - weather //config
-    configCurrentWeather()
+    configCurrentWeather(weather)
     console.log(weather)
 
 }
