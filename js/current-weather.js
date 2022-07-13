738599cb73630075df35daf7508ed0fec7172426
Esteban Pajares <jehupajares@gmail.com>
2022-07-13 00:02:57 -0500

    Geolocalizacion y Callbacks

--- js/current-weather.js
+++ js/current-weather.js
@@ -1,6 +1,7 @@
 import weather from '../data/current-weather.js' //importando data del clima
 import { formatDate, formatTemp } from './utils/format-data.js'
 import { weatherConditionsCodes } from './constants.js'
+import {getCurrentPosition} from './geolocation.js'
 
 //weatherConditionsCodes[]
 
@@ -67,6 +68,7 @@ function configCurrentWeather(weather){
 }
 export default function currentWeather(){
     //GEO //API - weather //config
+    getCurrentPosition()
     configCurrentWeather(weather)
     console.log(weather)
 
