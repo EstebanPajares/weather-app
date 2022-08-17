1eeca18cf83bf2ea2f051732c55ca602a384bb9c
Esteban Pajares <jehupajares@gmail.com>
2022-08-17 01:07:15 -0500

    redenderizando los dias de la semana de manera dinaminaca

--- js/tabs.js
+++ js/tabs.js
@@ -1,1 +1,36 @@
 console.log('Renderizando días de la semana')
+
+const $tabContainer = document.querySelector('#tabs')
+
+const $tabList = $tabContainer.querySelectorAll('.tab')
+
+const today = new Date()
+let weekday = today.getDay()
+
+const week = [
+    'Domingo',
+    'Lunes',
+    'Martes',
+    'Miércoles',
+    'Jueves',
+    'Viernes', 
+    'Sábado'
+]
+
+function nextDay(day){
+    if(day === 6){
+        return 0
+    }
+    return day + 1
+}
+
+$tabList.forEach(($tab, index)=>{
+    if (index === 0){
+        $tab.textContent = 'Hoy'
+        weekday = nextDay(weekday)
+        return false
+    }
+    $tab.textContent = week[weekday]
+    weekday = nextDay(weekday)
+
+})
