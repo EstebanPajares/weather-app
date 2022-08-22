4003b3befd49f51101479ca3b1f47103a6e8200b
Esteban Pajares <jehupajares@gmail.com>
2022-08-22 01:02:18 -0500

    Definiendo el area-selected de los tabs

--- js/tabs.js
+++ js/tabs.js
@@ -39,6 +39,10 @@ $tabList.forEach(($tab, index)=>{
 
 function handleSelectTabClick(event){
     const $tabSelect = event.target
+    const $tabActive = document.querySelector('.tab[aria-selected="true"]')
+    $tabActive.removeAttribute('aria-selected')
+    $tabSelect.setAttribute('aria-selected', true)
+    
     const id = $tabSelect.id
     const $tabPanel = document.querySelector(`[aria-labelledby=${id}]`)
     const $tabPanelSelected = document.querySelector(`.tabPanel:not([hidden])`)
