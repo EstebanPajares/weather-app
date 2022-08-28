61069d10504944e03fb162992674d2ee817fc21e
Esteban Pajares <jehupajares@gmail.com>
2022-08-28 01:44:39 -0500

    Creando DOM a partir de strings -desde cero

--- js/weekly-weather.js
+++ js/weekly-weather.js
@@ -1,12 +1,19 @@
-// Prediccion de clima semanal
+//Pedir información del API
 import { getWeeklyWetaher } from "./services/weather.js";
 import {getLatLon} from './geolocation.js'
 import { formatWeekList } from './utils/format-data.js'
 
-function configWeeklyWeather(){
+function configWeeklyWeather(weeklist){
+    const $container = document.querySelector('.weeklyWeather')
+    weeklist.forEach((item)=>{
+        const $el = document.createElement('h2')
+        $el.textContent = 'Hola Mundo'
+        $container.append($el)
+    })
 
 }
 
+// Prediccion de clima semanal
 export default async function weeklyWeather(){
     const  { lat, lon, isError } =  await getLatLon()
     if (isError) return console.log('Ha ocurrido un error ubicandote')
@@ -14,6 +21,5 @@ export default async function weeklyWeather(){
     const { isError: weeklyWeatherError, data: weather} = await getWeeklyWetaher(lat, lon)
     if (weeklyWeatherError) return console.log('Oh! ha ocurrido un error trayendo los datos del pronóstico del clima')
     const weeklist = formatWeekList(weather.list)
-    debugger
-    configWeeklyWeather(weather)
+    configWeeklyWeather(weeklist)
 }
