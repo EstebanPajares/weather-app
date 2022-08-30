f9b755dfb6b81a57965141830a3dcf3fa506b3df
Esteban Pajares <jehupajares@gmail.com>
2022-08-30 00:39:33 -0500

    Renderizando los tabPanels

--- js/weekly-weather.js
+++ js/weekly-weather.js
@@ -4,14 +4,35 @@ import {getLatLon} from './geolocation.js'
 import { formatWeekList } from './utils/format-data.js'
 import { createDOM } from './utils/dom.js'
 
+//Renderizando los tabPanels
+function tabPanelTemplate(id){
+   return `
+    <div class="tabPanel" tabindex="0" aria-labelledby="tab-${id}">
+    <div class="dayWeather" id="dayWeather-${id}">
+      <ul style = "color:white" class="dayWeather-list" id="dayWeather-list-${id}">
+       Tab Panel ${id}
+      </ul>
+    </div>
+  </div>
+    `
+}
+
+function createTabPanel(id){
+    const $panel = createDOM(tabPanelTemplate(id))
+    if (id > 0){
+        $panel.hidden = true
+    }
+    return $panel
+}
+
 function configWeeklyWeather(weeklist){
     const $container = document.querySelector('.weeklyWeather')
-    weeklist.forEach((item)=>{
+    weeklist.forEach((day, index)=>{
         //const $el = document.createElement('h2')
         //$el.textContent = 'Hola Mundo'
         //$container.append($el) */
-        const $el = createDOM('<h2>Hola Mundo!</h2>')
-        $container.append($el)
+        const $panel = createTabPanel(index)
+        $container.append($panel)
     })
 
 }
