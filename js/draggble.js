790e7fd6ef46d6a41b0edecf95e2dae5b3bea10a
Esteban Pajares <jehupajares@gmail.com>
2022-09-05 00:45:30 -0500

    Definiendo el estado inicial del widget

--- js/draggble.js
+++ js/draggble.js
@@ -1,5 +1,5 @@
 const defaultConfig = {
-    open:true,
+    open:false,
     debug: true,
     animatable: true
 }
@@ -8,11 +8,43 @@ export default function draggble($element, config = defaultConfig){
         return console.warn(`Elemento invalido, se esperaba un HTMLElement y se recibio ${$element}`)
     }
     
+    let isOpen = config.open
+    const elementRect = $element.getBoundingClientRect()
+    const ELEMENT_BLOCK_SIZE = elementRect.height
+    
+    const $marker = $element.querySelector('[data-marker]')
+    const MARKER_BLOCK_SIZE = $marker.getBoundingClientRect().height
+    
+    const VISIBLE_Y_POSITION = 0
+    const HIDDEN_Y_POSITION = ELEMENT_BLOCK_SIZE - MARKER_BLOCK_SIZE
+    let widgetPosition = VISIBLE_Y_POSITION
+    
+    isOpen ? open() : close() //Op ternario
+
     //Validando los mensajes
     function logger(message){
         if(config.debug){
             console.info(message)
         }
+    }
+
+    function open(){
+        logger('Abrir widget')
+        isOpen = true
+        widgetPosition = VISIBLE_Y_POSITION
+        setWidgetPosition(widgetPosition)
+    }
+
+    function close(){
+        logger('Cerrar widget')
+        isOpen = false
+        widgetPosition = HIDDEN_Y_POSITION
+        setWidgetPosition(widgetPosition)
+    }
+
+    //Definiendo el estado inicial del widget
+    function setWidgetPosition(value){
+        $element.style.marginBottom = `-${value}px` //movemos el elemento en negativo Y
     }
 }
 
--- js/period-time.js
+++ js/period-time.js
@@ -7,11 +7,10 @@ export function periodTimeTemplate({temp, date, icon, description}){
     return `
     <li class="dayWeather-item is-selected">
         <span class="dayWeather-time">${date}</span>
-        <img class="dayWeather-icon" src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="${description}" rain="">
+        <img class="dayWeather-icon"  height="48" width="48" src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="${description}" rain="">
         <span class="dayWeather-temp">${temp}</span>
     </li>
     `
-
 }
 
 export function createPeriodTime(weather) {
